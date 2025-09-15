e075b3ee5a9445d3fb70382909d62999d08d4684
Phenyo <111151106+phenyo-droid@users.noreply.github.com>
2025-09-15 15:50:10 +0200

    update index.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,11 @@ import './App.css'
 import MovieCard from './components/MovieCard'
 
 function App() {
+  const movieNumber = 1;
 
   return (
    <>
-    <MovieCard movie={{title: "#", release_date: "2025"}}/>
+    {movieNumber === 1? <MovieCard movie={{title: "#", release_date: "2025"}}/>
    </>
   )
 }
